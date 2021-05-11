29b068cbebe988a105d0a047cd55554c7d47f6ba
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-11 18:16:46 +0530

    Spotify - Body part

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,10 @@ import './navbar.css'
 
 function Navbar() {
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className="navbar">
+            <Link to="/">
+                <i style={{ color: 'black' }} className="fas fa-typo3"></i>
+            </Link>
             <Link to="/">Home</Link>
             <Link to="/contact">Contact</Link>
             <Link to="/about">About</Link>
