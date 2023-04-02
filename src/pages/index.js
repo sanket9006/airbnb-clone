2dcd81d39f92ced22d1e19aa74abf20fc2af73ed
Sanket Patil <sanket.9006@gmail.com>
2023-04-02 20:43:16 +0530

    Updated readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import Navbar from '@/components/navbar/Navbar';
+// import Navbar from '@/components/navbar/Navbar';
 // import AirbnbBanner from '@/components/banner/AirbnbBanner';
 // import ExploreNearbyCard from '@/components/cards/ExploreNearbyCard';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home(props) {
-  console.log(props);
   return (
     <>
       <Head>
@@ -16,7 +13,7 @@ export default function Home(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <AirbnbBanner /> */}
 
       <main className="max-w-7xl mx-auto">
